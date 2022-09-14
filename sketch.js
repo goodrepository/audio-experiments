e35bc307fc58b8a40ec9e4856850a9972636514e
tj-parkwood <109317496+tj-parkwood@users.noreply.github.com>
2022-09-14 12:07:53 -0700

    Basic Animation

--- sketch.js
+++ sketch.js
@@ -5,62 +5,109 @@ let pos = {
   y: 0
 };
 
+let positionsX = [];
+let positionsY = [];
+let r = [];
+let g = [];
+let b = [];
+let a = [];
+
+let sound, amplitude, fft, size;
+
+function preload(){
+  sound = loadSound('./assets/music/jinsang-affection.m4a');
+}
 
 function setup () {
+    var canv = createCanvas(window.innerWidth, window.innerHeight);
+    var canvas = document.getElementsByTagName("canvas")[0];
+    console.log(document, canvas);
+    // console.log(gsap);
+    var ctx = canvas.getContext("2d");
 
-    var WIDTH = 3000;
-    var HEIGHT = 3000;
-    var NUMBER = 200;
-
-    // padding will increase the size of stage
-    // so scrolling will look smoother
-    var PADDING = 500;
-
-    var canvas = createCanvas((WIDTH + PADDING * 2), (HEIGHT + PADDING * 2));
-
-    console.log(gsap)
-
+    // canvas.mousePressed(togglePlay);
+    amplitude = new p5.Amplitude();
+    fft = new p5.FFT([1.0], 128);
+    sound.amp(0.2);
+    console.log(fft)
 
 
-    gsap.to(pos, {
-        x: 100,
-        y: 100,
-        duration: 2,
-        repeat: -1
-      });
+    for (let i = 0; i < 50; i++) {
+      var randomX = Math.floor(Math.random() * window.innerWidth);
+      var randomY = Math.floor(Math.random() * window.innerHeight);
+      var randomR = Math.floor(Math.random() * 255);
+      var randomG = Math.floor(Math.random() * 255);
+      var randomB = Math.floor(Math.random() * 255);
+      var randomA = Math.floor(Math.random() * 1);
+      var randomSizer = Math.floor(Math.random() * 10);
+      let c = color(randomR, randomG, randomB, [randomA]);
+      fill(c)
+      stroke(c)
+      ellipse(randomX, randomY, randomSizer * size, randomSizer * size);
+    }
 
+    console.log(randomX)
 
+    
 
-
-    /*********** SCROLL STUFF -- NO NEED TO TOUCH - TY! ************/
+    /*********** SCROLL AND CANVAS STUFF -- NO NEED TO TOUCH - TY! ************/
     var scrollContainer = document.getElementsByTagName("main")[0];
     var largeContainer = document.createElement("div");
     largeContainer.id = "large-container";
-    largeContainer.appendChild(canvas.canvas);
+    largeContainer.appendChild(canvas);
     // scrollContainer.remove(canvas.canvas);
     scrollContainer.appendChild(largeContainer);
-
-
-
-    function repositionStage() {
-        var dx = scrollContainer.scrollLeft - PADDING;
-        var dy = scrollContainer.scrollTop - PADDING;
-        console.log('canvas', canvas.canvas.style.transform)
-        canvas.canvas.style.transform = 'translate(' + dx + 'px, ' + dy + 'px)';
-        canvas.canvas.offsetLeft = -dx;
-        canvas.canvas.offsetTop = -dy;
-    // canvas.y(-dy);
-    }
-    scrollContainer.addEventListener('scroll', repositionStage);
-    repositionStage();
-    /*********** SCROLL STUFF -- NO NEED TO TOUCH - TY! ************/
+    /*********** SCROLL AND CANVAS STUFF -- NO NEED TO TOUCH - TY! ************/
 }
 
+function mousePressed() {
+  console.log('sound:', sound)
+  if (sound.isPlaying() ){
+    sound.pause();
+  } else {
+    sound.loop();
+		amplitude = new p5.Amplitude();
+		amplitude.setInput(sound);
+  }
+
+  drawCircles(50);
+}
+
+
 function draw () {
-    console.log(pos.x)
-    
-    // background(220);
-    fill(65);
-    text("p5 with gsap", pos.x, pos.y);
-    // console.log('title: ', title)
+
+  background(000);
+  text('tap to play', 20, 20);
+
+  let level = amplitude.getLevel();
+  size = map(level, 0, 1, 10, 400);
+  // ellipse(width/2, height/2, size, size);
+  // ellipse(20, 200, size * 3, size);
+
+  fill(255, 100, 105);
+  ellipse(width/2, height/2, size * 5, size * 5);
+
+
+  // let spectrum = fft.analyze();
+  // noStroke();
+  // fill(255, 0, 255);
+  // for (let i = 0; i< spectrum.length; i++){
+  //   let x = map(i, 0, spectrum.length, 0, width);
+  //   let h = -height + map(spectrum[i], 0, 255, height, 0);
+  //   rect(x, height, 20, h )
+  // }
+
+  let waveform = fft.waveform();
+  console.log(waveform)
+  noFill();
+  beginShape();
+  stroke("#fff");
+  for (let i = 0; i < waveform.length; i++){
+    let x = map(i, 0, waveform.length, 0, width);
+    let y = map( waveform[i], -1, 1, 0, height);
+    vertex(x,y);
+  }
+  endShape();
+
+
 } 
